4ba0e1e4f820e0abce9d469c983deb446786be8b
Andrii Bida <nomicos@users.noreply.github.com>
2016-10-17 23:14:29 +0200

    Fix the code so that it'd compile
    All occurrences of the `p` variable have been replaced with
    `priority` as it is the one that fits there logically.
    
    The <algorithm> header has been #included, so that the program
    could use the std::swap function. The arguments have been
    adjusted to meet the parameter list as well.

--- priorityScheduling.cpp
+++ priorityScheduling.cpp
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <algorithm>
 
 int main()
 {
@@ -20,31 +21,31 @@ int main()
 	for(i=0;i<n;i++)
 	{
 		pn[i]=i+1;
-		scanf("%d %d %d",&at[i],&bt[i],&p[i]);
+		scanf("%d %d %d",&at[i],&bt[i],&priority[i]);
 		finished[i]=0;
 		rm[i]=bt[i];
 
 	}
-	
-	
+
+
 	//sorting the processes on arrival time(ascending order)
-	
+
 	for( i=0;i<n;i++)
 	{
 		for(j=0;j<n-i-1;j++)
 		{
 			if(at[j]>at[j+1])
 			{
-				swap(&at[j],&at[j+1]);
-				swap(&bt[j],&bt[j+1]);
-				swap(&pn[j],&pn[j+1]);
+				std::swap(at[j],at[j+1]);
+				std::swap(bt[j],bt[j+1]);
+				std::swap(pn[j],pn[j+1]);
 			}
 		}
 
 	}
 
-	
-	
+
+
 	int total_time,all_fin=0;
 	int min;
 	int pos=0,flag=0;
@@ -61,9 +62,9 @@ int main()
 		for(j=0;j<n;j++)
 		{
 		   //checking for process that has arrived but not yet finished (having highest priority at that time)
-		    if(at[j]<=total_time && finished[j]==0 && p[j]<min)
+		    if(at[j]<=total_time && finished[j]==0 && priority[j]<min)
 		    {
-		        min=p[i];
+		        min=priority[i];
 		        pos=j;
 		        flag=1;
 		    }
@@ -106,12 +107,12 @@ int main()
 
 	}
 	printf("\n");
-	
+
 	//total_wt---total waiting time
 	//total_tat--total turn around time
 
 	float total_wt=0,total_tat=0;
-	
+
 	//calculating total turn around time, waiting time, totat waiting time and total turn around time using basic formule
 	for(i=0;i<n;i++)
 	{
@@ -120,12 +121,12 @@ int main()
 		total_wt+=wt[i];
 		total_tat+=tat[i];
 	}
-	
+
 
 	//printing arrival time, burst time, finish time, turn around time and waiting time for all processes in tabular form
 	printf("Process \t AT \t BT \t p \t FT \t TAT \t WT\n");
 	for(i=0;i<n;i++)
-		printf("P%d \t\t %d \t %d \t %d \t\t %d \t %d \t %d\n",pn[i],at[i],bt[i],p[i],ft[i],tat[i],wt[i]);
+		printf("P%d \t\t %d \t %d \t %d \t\t %d \t %d \t %d\n",pn[i],at[i],bt[i],priority[i],ft[i],tat[i],wt[i]);
 
 	//calculating average turn around time and average waiting time
 	printf("\nAverage TAT: %f\n",total_tat/n);
