f4ef989055712cf607dec55a54d565104e3401fe
HackersSpirit <mishrasatyam02@gmail.com>
2016-10-18 01:41:54 +0530

    Added/Modified the documentation of code

--- priorityScheduling.cpp
+++ priorityScheduling.cpp
@@ -1,21 +1,35 @@
+/**
+	Program-1
+	priorityScheduling.cpp
+	Purpose:To implement priority scheduling Algorithm.
+
+	@aurthor Shilpi
+	@version 1.0 18/10/16
+*/
+
 #include<stdio.h>
 
+//Controls operation of the program
 int main()
 {
 	int n,i,j,k;
-	/*
-	at---arrival time
-	bt--burst time
-	ft---finish time
-	tat---turn around time
-	rm---remaining time
-	finished----to check if process is finished
-	*/
+/*
+    Given below are some conventions.
+
+	@at Arrival Time.
+	@bt Burst Time. 
+	@ft Finish Time. 
+	@tat Turn Around Time.
+	@rm Remaining Time. 
+	@finished To check process completion state.
+*/
 	int at[20],bt[20],ft[20],pn[20],tat[20],wt[20],finished[20],rm[20],priority[20];
 	printf("Enter the number of processes: ");
 	scanf("%d",&n);
-
-	//taking arrival time and burst time for n processes and initialing isFinished to false and remaining time to burst time
+	
+	//Input arrival time and bust time for n process.
+	//Initialize isFinished to false.
+	//and remaining time to burst time.
 	printf("Enter at, bt and priority of processes: \n");
 	for(i=0;i<n;i++)
 	{
@@ -26,9 +40,7 @@ int main()
 
 	}
 	
-	
-	//sorting the processes on arrival time(ascending order)
-	
+	//Ascending order sorting of process on arrival time.	
 	for( i=0;i<n;i++)
 	{
 		for(j=0;j<n-i-1;j++)
@@ -43,17 +55,18 @@ int main()
 
 	}
 
-	
-	
 	int total_time,all_fin=0;
 	int min;
 	int pos=0,flag=0;
 
 	 total_time=at[0];
 
+/**
+	While process is running find the highest priority process and process it.
+	Keep checking the upcoming process at regular interval for higher priority.
 
-	//while all processes are not finished, find the process with lowest ie highest priority and process it.At each interval keep checking the upcoming processes for higher priority
-	//total_time----total time elapsed
+	@total_time Total Time Elapsed
+*/
 	while(!all_fin)
 	{
 		min=99999;
@@ -106,13 +119,14 @@ int main()
 
 	}
 	printf("\n");
-	
-	//total_wt---total waiting time
-	//total_tat--total turn around time
 
+/**
+	@total_wt Total Waiting Time
+	@total_tat Total Turn Around Time
+*/
 	float total_wt=0,total_tat=0;
 	
-	//calculating total turn around time, waiting time, totat waiting time and total turn around time using basic formule
+	//calculating total turn around time, waiting time, totat waiting time and total turn around time using basic formule.
 	for(i=0;i<n;i++)
 	{
 		tat[i]=ft[i]-at[i];
@@ -122,12 +136,12 @@ int main()
 	}
 	
 
-	//printing arrival time, burst time, finish time, turn around time and waiting time for all processes in tabular form
+	//printing arrival time, burst time, finish time, turn around time and waiting time for all processes in tabular form.
 	printf("Process \t AT \t BT \t p \t FT \t TAT \t WT\n");
 	for(i=0;i<n;i++)
 		printf("P%d \t\t %d \t %d \t %d \t\t %d \t %d \t %d\n",pn[i],at[i],bt[i],p[i],ft[i],tat[i],wt[i]);
 
-	//calculating average turn around time and average waiting time
+	//calculating average turn around time and average waiting time.
 	printf("\nAverage TAT: %f\n",total_tat/n);
 	printf("Average WT: %f\n",total_wt/n);
 }
